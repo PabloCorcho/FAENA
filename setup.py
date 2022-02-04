534ac2432999cc234cc7fc8e00f6de2b1cf075be
PabloCorcho <corchocaballeropablo@gmail.com>
2022-02-04 15:02:17 +1100

    New setup file for installation

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FAENA-PabloCorcho",
+    version="0.0.1",
+    author="PabloCorcho",
+    author_email="corchocaballeropablo@gmail.com",
+    description="integral Field Analysis for Extended gAlaxies (FAENA)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/PabloCorcho/FAENA",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
